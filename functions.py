1d02267c5f58ccfa6123a98f4c6ed39480a6d3a3
tai95 <59809187+tai95@users.noreply.github.com>
2020-10-14 10:14:05 -0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,21 @@
+#Functions 
+
+print ("Here is a example")  #this is a function 
+def who_am_i():
+    name = "Joe"
+    age = 30
+    print ("My name is" + name + "and I am" + str(age) + "years old" )
+    
+who_am_i() 
+
+def add_one_hundred(num): 
+    print(num + 100)
+    
+    
+add_one_hundred(100)
+
+
+def multiply(x,y):
+     return x * y
+    
+print(multiply(1,10))
